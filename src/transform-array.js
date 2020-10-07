3d8656272226d84626ed3a8a3b8576d28dc410e3
Inna <inna.rekesh@gmail.com>
2020-10-07 21:04:28 +0300

    feat: implement task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('is not Array')
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i]
+    switch (item) {
+      case '--double-next':
+        if (i !== arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
+        break
+      case '--double-prev':
+        if (i !== 0) {
+          result.push(arr[i- 1]);
+        }
+        break
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        result.pop();
+        break;
+      default:
+        result.push(item);
+    }
+  }
+  return result
+}
