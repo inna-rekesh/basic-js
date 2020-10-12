bc080c7fd6feab971081283f3df1766901a892a4
Inna <inna.rekesh@gmail.com>
2020-10-12 21:29:53 +0300

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,8 +1,36 @@
-module.exports = function repeater(/*str, options*/) {
-  /*let result = ''
-  for (let i = 0; i < options.additionRepeatTimes; i++) {
-    result += `${str}${options.addition}${options.additionSeparator}${options.separator}`
+module.exports = function repeater(str, options) {
+  const defaultSeparator = '+';
+  const defaultAdditionSeparator = '|';
+  let item = str;
+  let resultAddition = [];
+  let result = []
+  if(options.addition || options.addition === false || options.addition === null) {
+    for (let i = 0; i < options.additionRepeatTimes-1; i++) {
+      if(options.additionSeparator) {
+        resultAddition.push(''+options.addition, options.additionSeparator)
+      }else {
+        resultAddition.push(''+options.addition, defaultAdditionSeparator)
+      }
+    }
+    resultAddition.push(''+options.addition)
+    item += resultAddition.join('')
+    for (let i = 0; i < options.repeatTimes - 1; i++) {
+      if(options.separator) {
+        result.push(item, options.separator)
+      } else {
+        result.push(item, defaultSeparator)
+      }
+    }
+    result.push(item)
+  } else {
+    for (let i = 0; i < options.repeatTimes - 1; i++) {
+      if(options.separator) {
+        result.push(item, options.separator)
+      } else {
+        result.push(item, defaultSeparator)
+      }
+    }
+    result.push(item)
   }
-console.log(result)
-  return result;*/
+  return result.join('');
 };
