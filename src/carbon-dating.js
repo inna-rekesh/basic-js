b9b2be6a5196488c637090110a3a6b4131c01e80
Inna <inna.rekesh@gmail.com>
2020-10-16 22:26:18 +0300

    feat: implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,6 +1,11 @@
-const MODERN_ACTIVITY= 15;
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/*sampleActivity*/) {
-
+module.exports = function dateSample(sampleActivity) {
+  const sampleActivityP = parseFloat(sampleActivity)
+  if (typeof (sampleActivity) === 'string' && sampleActivityP > 0 &&
+    sampleActivityP < MODERN_ACTIVITY) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityP) / (0.693 / HALF_LIFE_PERIOD));
+  }
+  return false
 };
