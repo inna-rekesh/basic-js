46ffa02bdfad20af0e6f30e63fcb567779dfb376
Inna <inna.rekesh@gmail.com>
2020-10-16 22:28:13 +0300

    fix: fix problem dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,6 @@
 module.exports = function createDreamTeam(members) {
   if(!Array.isArray(members)) return false
-  return members.filter(item => typeof item === "string").map(str=>str.trim()[0]).sort().join('').toUpperCase()
+  return members.filter(item => typeof item === "string")
+    .map(str=>(str.trim()[0].toUpperCase())).sort().join('')
 }
 
